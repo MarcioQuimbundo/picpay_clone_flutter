9050dd9b9505cbb1b594b47940b2b1ff9849c4a2
Leonardo Benedeti <leonardobenedeti@gmail.com>
2019-10-22 11:52:11 -0300

    Criação do projeto com home separada do main e inclusao de leading e action icons

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:picpay_clone_flutter/pages/home/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primaryColor: Colors.grey[200],
+        accentColor: Color(0xff11C76F),
+        cardColor: Colors.white
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
---
+++ lib/pages/home/home.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.account_balance_wallet),
+          onPressed: ()=>print("tap leading"),
+        ),
+        title: Text("Incluir saldo"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.person_add),
+            onPressed: ()=>print("tap right button"),
+          )
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
